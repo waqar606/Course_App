69ea406c363559919fb8d554f1af6cabb68511f5
unknown <waqar.christiano@gmail.com>
2025-05-25 20:15:44 -0500

    changing frontend

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -5,6 +5,10 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
+import axios from "axios";
+
+// Configure axios defaults
+axios.defaults.withCredentials = true;
 
 const stripePromise = loadStripe(
   "pk_test_51RBQtmP7VxxtVS3zp8rUsgBKiLNSFUQ3Uq33hXtdpAjTbpJgvG2bl0Fg1wrjTpe71aiuPKsdFGpdSG5rvJbN7IQs00FGXWtq2q"
